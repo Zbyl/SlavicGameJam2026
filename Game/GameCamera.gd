0f3888079b231bd2446132be946154e815343342
Jan Skowron <jan.skowron@gmail.com>
2026-08-08 21:55:40 +0200

    Fix for camera zoom for warious window sizes

--- Game/GameCamera.gd
+++ Game/GameCamera.gd
@@ -26,16 +26,17 @@ func _process(delta: float) -> void:
         
     var stdOfPosition = Vector2.ZERO
     var zoomIncreaseMaxRelVelocity =  0.05
-    var viewWidthAtZoom0 = 1280.0
-    var viewHeightAtZoom0 = 640.0
-    
+    var screen_size = get_viewport().get_visible_rect().size
+    var viewWidthAtZoom0 = screen_size.x
+    var viewHeightAtZoom0 = screen_size.y
+
     if dudes.size() > 0:
         averagePosition /= dudes.size()
         averagePosition2 /= dudes.size()
         stdOfPosition.x = sqrt(averagePosition2.x - averagePosition.x ** 2) 
         stdOfPosition.y = sqrt(averagePosition2.y - averagePosition.y ** 2) 
-        var neededZoomX = viewWidthAtZoom0 / max(maxPosOfKuns.x - minPosOfKuns.x + 0.05 * viewWidthAtZoom0, 3 * stdOfPosition.x)
-        var neededZoomY = viewHeightAtZoom0 / max(maxPosOfKuns.y - minPosOfKuns.y + 0.05 * viewHeightAtZoom0, 3 * stdOfPosition.y)
+        var neededZoomX = viewWidthAtZoom0 / max(maxPosOfKuns.x - minPosOfKuns.x + 0.05 * viewWidthAtZoom0, 2.5 * stdOfPosition.x)
+        var neededZoomY = viewHeightAtZoom0 / max(maxPosOfKuns.y - minPosOfKuns.y + 0.05 * viewHeightAtZoom0, 2.5 * stdOfPosition.y)
         var neededZoom = min(1.0, neededZoomX, neededZoomY)
         position = averagePosition
         var oldZoom = zoom.x
