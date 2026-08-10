abe9b518a81c4c75db546128bc6cb8eec814e17f
Zbigniew Skowron <zbychs@gmail.com>
2026-08-10 19:17:00 +0200

    Simpler trash.gd

--- Game/trash.gd
+++ Game/trash.gd
@@ -1,17 +1,12 @@
 extends RigidBody2D
+class_name Trash
 
-@onready var ball: Sprite2D = $BallContainer/Ball
-@onready var leaf4: Sprite2D = $Leaf4Container/Leaf4
-@onready var ballContainer: Node2D = $BallContainer
-@onready var leaf4Container: Node2D = $Leaf4Container
+@onready var image: Sprite2D = $Leaf4Container/Leaf4
+@onready var imageContainer: Node2D = $Leaf4Container
 
-@export var isBall: bool = false
 var myLayer: int = 0
 
-var ballBasePosition: Vector2
-var leaf4BasePosition: Vector2
-var ballContainerBasePosition: Vector2
-var leaf4ContainerBasePosition: Vector2
+var imageContainerBasePosition: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,27 +18,10 @@ func _ready() -> void:
     z_index = layer
     set_collision_mask_value(17 + layer, true)
 
-    if isBall:
-        ball.visible = true
-        leaf4.visible = false
-        linear_damp = 1.0
-        angular_damp = 0.05
-    else:
-        physics_material_override = null
-        ball.visible = false
-        leaf4.visible = true
-
-    ballBasePosition = ball.position
-    leaf4BasePosition = leaf4.position
-    ballContainerBasePosition = ballContainer.position
-    leaf4ContainerBasePosition = leaf4Container.position
+    imageContainerBasePosition = imageContainer.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-    ballContainer.global_position = global_position + ballContainerBasePosition
-    ballContainer.global_rotation_degrees = 0
-    ball.rotation = self.rotation
-
-    leaf4Container.global_position = global_position + leaf4ContainerBasePosition
-    leaf4Container.global_rotation_degrees = 0
-    leaf4.rotation = self.rotation
+    imageContainer.global_position = global_position + imageContainerBasePosition
+    imageContainer.global_rotation_degrees = 0
+    image.rotation = self.rotation
